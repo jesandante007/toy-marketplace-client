6226c35d6785a52afb84ddbe6fb9889763e20230
jesandante007 <jesandante007@gmail.com>
2023-05-19 16:28:59 +0600

    toyDetails page done

--- src/pages/ToyDetails.jsx
+++ src/pages/ToyDetails.jsx
@@ -13,26 +13,34 @@ const ToyDetails = () => {
     image,
     features,
     rating,
+    email
   } = toy;
   return (
     <div className="hero my-bg">
       <div className="hero-content flex-col lg:flex-row">
         <img src={image} className="w-full max-w-lg rounded-lg shadow-2xl" />
         <div>
-          <h1 className="text-4xl font-bold text-tdeep">{name}</h1>
-          <p className="mt-5 text-2xl">Price: ${price}</p>
+          <h1 className="text-4xl font-bold text-tdeep mb-1">{name}</h1>
+          <p>Category: {category}</p>
+          <p className="mt-5 text-2xl">Price: <span className="text-tdeep">${price}</span></p>
           <div className="flex items-center gap-5 mt-1">
             <div className="flex gap-1">
               <span>Rating:</span>{" "}
-              <Rating style={{ maxWidth: 120 }} value={rating} readOnly />
+              <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
             </div>
-            <p className="px-2 my-bg">{rating}</p>
+            <p className="px-2 my-bg text-sm text-tdeep">{rating}</p>
           </div>
-          <p className="mt-3">Features:</p>
+          <p>Available Quantity: {availableQuantity}</p>
+          <p className="mt-4 text-lg">Features:</p>
           <ul className="list-disc ml-5">
             {
                 features.map((feature, index) => <li key={index}>{feature}</li>) 
             }
+          </ul>
+          <p className="mt-4 text-lg">Seller Info: </p>
+          <ul className="list-disc ml-5">
+            <li>Name: {seller}</li>
+            <li>Email: {email ? email : 'Not Available'}</li>
           </ul>
         </div>
       </div>
